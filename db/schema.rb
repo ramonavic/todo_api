86cd0da7c3cac518b36be119d261054c2e0f39e5
ramonavic <ramon.hofmeister@gmail.com>
2016-07-06 21:10:51 +0200

    migrated default value

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706151044) do
+ActiveRecord::Schema.define(version: 20160706174226) do
 
   create_table "todos", force: :cascade do |t|
     t.string   "description"
-    t.boolean  "completed"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.boolean  "completed",   default: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
 end
