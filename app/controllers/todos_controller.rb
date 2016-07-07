5eda28b822d475dcadc023b941187f67924d6443
ramonavic <ramon.hofmeister@gmail.com>
2016-07-07 20:17:12 +0200

    cucumber and json spec gem

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,4 +7,37 @@ class TodosController < ApplicationController
     render json: @todos
   end
 
+  def show
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      json { render :show, status: :created, location: @todo }
+    else
+      json { render json: @todo.errors, status: :unprocessable_entity }
+    end
+  end
+
+  def delete
+    @todo.destroy
+  end
+
+  def update
+    if @todo.update(todo_params)
+      json { render :show, status: :ok, location: @todo }
+    else
+      json { render json: @todo.errors, status: :unprocessable_entity }
+    end
+  end
+
+
+  private
+
+  def todo_params
+      params.require(:todo).permit(:description, :completed)
+  end
+
+
 end
